ab667773f68628c5edbb87cc1b9161ed11c84309
Meghraj <meghraj.deshmukh103@gmail.com>
2022-03-14 23:39:38 +0530

    redux thunk added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,26 +3,26 @@
 import { addData } from '../FireBase/FireBase';
 import { saveData } from './action';
 import  rootReducer from './reducer';
+import thunk from 'redux-thunk'
 
 
+const thunkMiddleware =
+({ dispatch, getState }:{dispatch:any,getState:any}) =>
+(next:any) =>
+(action:any) => {
+  // If the "action" is actually a function instead...
+  if (typeof action === 'function') {
+    // then call the function and pass `dispatch` and `getState` as arguments
+   // return action(dispatch, getState)
+  console.log("....typof action is function.....");
+  }
+   console.log("Normal  action");
+  // Otherwise, it's a normal action - send it onwards
+  return next(action);
 
-function logger(state:any) {
-    return (next:any) => (action:any) => {
-       console.log('action', next);
-       const returnVal = next(action);
-       console.log("action",action);
-       console.log('state when action is dispatched',state.getState()); 
-       addData(action.payload).then((data)=>{
-           console.log("saved data",data);
-       });
-       next(action);
-      // return returnVal;
-        
-        
-    }
- }
+}
 
- const store= createStore(rootReducer,applyMiddleware(logger));
+ const store= createStore(rootReducer,applyMiddleware(thunk));
 console.log("store ",store.getState());
-  store.dispatch(logger);
+  store.dispatch(thunkMiddleware);
    export default store;
